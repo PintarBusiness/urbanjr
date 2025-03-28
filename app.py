d16674562b290846b0551ffbb651a2be1b66cc07
Aljaz55 <strzlaljaz@gmail.com>
2025-03-28 16:22:23 +0000

    admin slog malo sprememb

--- app.py
+++ app.py
@@ -44,6 +44,3 @@ def narocila():
 app.run(debug = True)
 
 
-"""
-we have our website uploaded on github. our link is https://pintarbusiness.github.io/urbanjr/ . The link worked as long as we did not make the right structure. When index.html was outside the templates folder it worked but when we placed it into te folder it sttoped working
-"""
