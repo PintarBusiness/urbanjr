31fbf4f4080ea87aa74b85657e68b1816a543d2e
Aljaz55 <strzlaljaz@gmail.com>
2025-03-28 07:50:21 +0000

    spremenjeno v standard flaska

--- app.py
+++ app.py
@@ -9,9 +9,37 @@ app = Flask(__name__)
 
 @app.route("/")
 
-def api_poskus():
-    print("skoz")
+def index():
     return render_template("index.html")
+
+@app.route("/onas")
+
+def onas():
+    return render_template("onas.html")
+
+@app.route("/trgovina")
+
+def trgovina():
+    return render_template("trgovina.html")
+
+@app.route("/blog")
+
+def blog():
+    return render_template("blog.html")
+
+@app.route("/kontakt")
+
+def kontakt():
+    return render_template("kontakt.html")
+
+@app.route("/admin")
+
+def admin():
+    return render_template("admin.html")
+
+@app.route("/narocila")
+def narocila():
+    return render_template("narocila.html")
 
 app.run(debug = True)
 
