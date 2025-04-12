0dc930af4a2c0479fa39f49199d0329852b97faa
Aljaz55 <strzlaljaz@gmail.com>
2025-04-12 10:35:41 +0000

    dodane stvari od jureta kosarica... zacetek instagram api...

--- app.py
+++ app.py
@@ -6,25 +6,26 @@ import requests
 import random
 import redis
 import os
-
 #import requests
+
+# ---------- NALOZI  ----------
 
 #pip install flask --user
 #pip install redis
 #pip install tinydb
 
 app = Flask(__name__)
-app.secret_key = os.urandom(24)
-narocnikiDB = TinyDB('db.json')
 
-app.config['SESSION_TYPE'] = 'filesystem'  # or 'null'
+# ---------- Funkcije  za admin Session ----------
+app.secret_key = os.urandom(24)
+
+
+app.config['SESSION_TYPE'] = 'filesystem' 
 app.config['SESSION_PERMANENT'] = False
 
 @app.before_request
 def make_session_non_permanent():
     session.permanent = False
-
-
 
 @app.before_request
 def initialize_admin_mode():
@@ -32,49 +33,44 @@ def initialize_admin_mode():
         session['admin_mode'] = False
         session['last_log_link'] = "/"
 
+# ---------- Databaze ----------
+narocnikiDB = TinyDB('db.json')
 
-
+# ---------- Linki (poti) do html datotek ----------
 @app.route("/")
-
 def index():
     admin_mode = session.get('admin_mode', False)
     return render_template("index.html",admin_mode=admin_mode)
 
 @app.route("/onas")
-
 def onas():
     admin_mode = session.get('admin_mode', False)
     return render_template("onas.html",admin_mode=admin_mode)
 
 @app.route("/trgovina")
-
 def trgovina():
     admin_mode = session.get('admin_mode', False)
     return render_template("trgovina.html",admin_mode=admin_mode)
-
+"""
 @app.route("/blog")
-
 def blog():
     admin_mode = session.get('admin_mode', False)
     return render_template("blog.html",admin_mode=admin_mode)
-
+"""
 @app.route("/kontakt")
-
 def kontakt():
     admin_mode = session.get('admin_mode', False)
     return render_template("kontakt.html",admin_mode=admin_mode)
 
 @app.route("/admin")
-
 def admin():
-    referring_url = request.referrer or "/"  # Default to "/" if no referrer
+    referring_url = request.referrer or "/"  
     session['last_log_link'] = referring_url
     return render_template("admin.html")
 
 @app.route("/logout")
-
 def logout():
-    referring_url = request.referrer or "/"  # Default to "/" if no referrer
+    referring_url = request.referrer or "/" 
     session['last_log_link'] = referring_url
     return render_template("logout.html")
 
@@ -82,6 +78,8 @@ def logout():
 def narocila():
     admin_mode = session.get('admin_mode', False)
     return render_template("narocila.html",admin_mode=admin_mode)
+
+# ---------- Prijava v admin ----------
 
 userDict = {}
 passwordDict = {}
@@ -91,26 +89,30 @@ passwordDict["admin"] = 1
 
 @app.route("/loginTry", methods=["POST"])
 def login():
-    ime = request.form.get("ime")  # Get 'ime' from the POST request
-    geslo = request.form.get("geslo")  # Get 'geslo' from the POST request
+    ime = request.form.get("ime") 
+    geslo = request.form.get("geslo")  
 
     if ime in userDict and geslo in passwordDict:
         if userDict[ime] == passwordDict[geslo]:
             session['admin_mode'] = True
-            return jsonify({"redirect_to": session['last_log_link']})  # URL to redirect to
+            return jsonify({"redirect_to": session['last_log_link']})  
         else:
             return jsonify({"error": "Vnešeno ime ali geslo je napačno"}), 400
     else:
         return jsonify({"error": "Vnešeno ime ali geslo je napačno"}), 400
-    
+
+# ---------- Odjava iz admina ----------
+
 @app.route("/logoutSession", methods=["POST"])
 def logoutSession():
     session['admin_mode'] = False
-    return jsonify({"redirect_to": session['last_log_link']})  # URL to redirect to
+    return jsonify({"redirect_to": session['last_log_link']}) 
 
 @app.route("/goBack", methods=["POST"])
 def goBack():
-    return jsonify({"redirect_to": session['last_log_link']})  # URL to redirect to
+    return jsonify({"redirect_to": session['last_log_link']})  
+
+# ---------- Dodajanje e-mail racuna v databazo TinyDB (za novice) ----------
 
 @app.route("/poskusDodajanjaMail", methods=["POST"])
 def poskusDodajanjaMail():
@@ -124,6 +126,140 @@ def poskusDodajanjaMail():
         return jsonify(success=True)
     else:
         return jsonify(success=False)
+
+# ---------- Delovanje kosarice ----------
+
+izdelek_podatki = {
+    "JAJCA": {
+        "html": '''
+        <div class="sliketext">
+            <div class="text">
+                <h1>JAJCA</h1>
+                <a href="/odstrani_iz_kosarice/JAJCA" class="gumbnarocila"><span>ODSTRANI IZDELEK</span></a>
+            </div>
+            <div class="slika">
+                <img src="/static/images/trgovina/DSC07972-20.JPG" alt="Jajca">
+            </div>
+        </div>
+        '''
+    },
+    "PIŠČANCI": {
+        "html": '''
+        <div class="sliketext">
+            <div class="text">
+                <h1>PIŠČANCI</h1>
+                <a href="/odstrani_iz_kosarice/PIŠČANCI" class="gumbnarocila"><span>ODSTRANI IZDELEK</span></a>
+            </div>
+            <div class="slika">
+                <img src="/static/images/trgovina/DSC07979-21.JPG" alt="Piščanci">
+            </div>
+        </div>
+        '''
+    },
+    "MLEKO": {
+        "html": '''
+        <div class="sliketext">
+            <div class="text">
+                <h1>MLEKO</h1>
+                <a href="/odstrani_iz_kosarice/MLEKO" class="gumbnarocila"><span>ODSTRANI IZDELEK</span></a>
+            </div>
+            <div class="slika">
+                <img src="/static/images/trgovina/DSC08006-26.JPG" alt="Mleko">
+            </div>
+        </div>
+        '''
+    },
+    "ZELENJAVA": {
+        "html": '''
+        <div class="sliketext">
+            <div class="text">
+                <h1>ZELENJAVA</h1>
+                <a href="/odstrani_iz_kosarice/ZELENJAVA" class="gumbnarocila"><span>ODSTRANI IZDELEK</span></a>
+            </div>
+            <div class="slika">
+                <img src="/static/images/trgovina/DSC07972-20.JPG" alt="Zelenjava">
+            </div>
+        </div>
+        '''
+    },
+    "GOVEDINA": {
+        "html": '''
+        <div class="sliketext">
+            <div class="text">
+                <h1>GOVEDINA</h1>
+                <a href="/odstrani_iz_kosarice/GOVEDINA" class="gumbnarocila"><span>ODSTRANI IZDELEK</span></a>
+            </div>
+            <div class="slika">
+                <img src="/static/images/trgovina/DSC07979-21.JPG" alt="Govedina">
+            </div>
+        </div>
+        '''
+    }
+}
+
+@app.route("/dodaj_v_kosarico/<izdelek>")
+def dodaj_v_kosarico(izdelek):
+    kosarica = session.get("kosarica", [])
+    if izdelek.upper() not in kosarica:
+        kosarica.append(izdelek.upper())
+    session["kosarica"] = kosarica
+    return redirect(url_for("kosarica"))
+
+@app.route("/odstrani_iz_kosarice/<izdelek>")
+def odstrani_iz_kosarice(izdelek):
+    kosarica = session.get("kosarica", [])
+    izdelek_upper = izdelek.upper()
+    if izdelek_upper in kosarica:
+        kosarica.remove(izdelek_upper)
+    session["kosarica"] = kosarica
+    return redirect(url_for("kosarica"))
+
+@app.route("/kosarica")
+def kosarica():
+    admin_mode = session.get('admin_mode', False)
+    izbrani_izdelki_imena = session.get("kosarica", [])
+
+    izbrani = []
+    neizbrani = []
+
+    for ime, podatki in izdelek_podatki.items():
+        html = podatki["html"]
+        if ime in izbrani_izdelki_imena:
+            izbrani.append(html)
+        else:
+            html_zamenjan = html.replace(
+                f"/odstrani_iz_kosarice/{ime}", f"/dodaj_v_kosarico/{ime}"
+            ).replace("ODSTRANI IZDELEK", "DODAJ&nbsp;IZDELEK")
+            neizbrani.append(html_zamenjan)
+
+    return render_template("kosarica.html", izbrani=izbrani, neizbrani=neizbrani ,admin_mode=admin_mode)
+
+igtoken = ""
+iguporabnik = ""
+igapi = ""
+
+def instagramapi():
+    objave = {
+        "fields": "id,caption,media_type,media_url,permalink",
+        "access_token": igtoken,
+        "limit": 6
+    }
+
+    odgovor = requests.get(igapi, params=objave)
+    return odgovor.json().get("data", [])
+
+@app.route("/blog")
+
+def blog():
+    poslji = instagramapi
+    admin_mode = session.get('admin_mode', False)
+    return render_template("blog.html", post=poslji,admin_mode=admin_mode)
+
+@app.route("/api/poslji")
+
+def apiposlji():
+    poslji = instagramapi
+    return jsonify(poslji)
 
 app.run(debug = True)
 
